f53a65a84c81d0dae7c8b089a37efa706e00e77d
Francisco Yemail <gartija@gmail.com>
2012-12-02 23:58:57 -0500

    Cambios al home y a estilos para incluir el logo

--- wp-content/themes/autofocuslite/header.php
+++ wp-content/themes/autofocuslite/header.php
@@ -65,11 +65,9 @@
 				<hgroup>
 				    <?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div';?>
 					<<?php echo $heading_tag; ?> id="site-title">
-						<span>
-							<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-						</span>
-					</<?php echo $heading_tag; ?>>
-					<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+					<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+                    	<img src="<?php echo get_template_directory_uri(); ?>/images/logo-metropolitana.jpg"/>
+                    </a>
 				</hgroup>
 			</div><!-- #branding -->
 
